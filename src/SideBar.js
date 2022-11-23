a3f053b86c7fbfbaac74958d37d8fa056a9b75ce
Pablo Belfort <pablobelfort@gmail.com>
2022-11-23 15:25:37 -0300

    layout entrar e cadastrar

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,21 +1,39 @@
+import { useState } from "react"
 import styled from "styled-components"
 
 
 export default function SideBar () {
-    return (
-        <Bar>
 
+    const [show, setShow] = useState(false)
+
+    return (
+        <Bar show={show}>
+            <ion-icon name="list-outline" onClick={()=>setShow(!show)}></ion-icon>
+            <Category></Category>
         </Bar>
     )
 }
 
 const Bar = styled.aside`
     display: flex;
+    align-items: center;
+    padding: 10px;
     flex-direction: column;
     position: fixed;
     top: 0;
     left: 0;
-    width: 5vw;
+    width: ${props => props.show ===false ? '4vw' : '10vw'};
     height: 100vh;
     background-color: #181824;
+    
+    ion-icon{
+        color: white;
+        width: 40px;
+        height: 40px;
+        cursor: pointer;
+    }
+`
+
+const Category = styled.div`
+
 `
